cbb9a0c912eb1a0e792f1f0b2ebef4c0daac98e2
Anthony Pucelle <anthony@presentagency.com>
2018-09-12 11:38:05 +0100

    Update Ajax calls to use the same file

--- www/block/updateChart.php
+++ www/block/updateChart.php
@@ -7,11 +7,12 @@ include '../block/bddConnexion.php';
 if(isset($_POST['function2call']) && !empty($_POST['function2call'])) {
 
     $function2call = $_POST['function2call'];
+    $currentYear    = date("Y");
+    $currentMonth  	= date("m");
+    $currentDay     = date('d');
+    $previousMonth  = $currentMonth - 1;
 
     if(!isset($_POST['start']) OR !isset($_POST['end'])) {
-
-        $currentYear    = date("Y");
-        $currentMonth  	= date("m");
 
         $startDate      = $currentYear . '-' . $currentMonth . '-01';
         $endDate        = $currentYear . '-' . $currentMonth . '-31';
@@ -23,9 +24,24 @@ if(isset($_POST['function2call']) && !empty($_POST['function2call'])) {
 
     }
 
+    if(!isset($_POST['year'])){
+        $year = $currentYear;
+    }else{
+        $year = $_POST['year'];
+    }
+
+    $category   = $_POST['category'];
+    $type       = $_POST['type'];
+
+    $render = new render();
+
     switch($function2call) {
-        case 'spendingMonthToDate' : $render = new render(); print $render->spendingMonthToDateCategory("1", $startDate, $endDate); break;
-        case 'other' : // do something;break;
+        case 'spendingMonthToDateCategory'      : print $render->spendingMonthToDateCategory("1", $startDate, $endDate); break;
+        case 'spendingMonthToDate'              : print json_encode($render->spendingMonthToDate("1", $currentYear, $currentMonth, $currentDay)); break;
+        case 'yearSpendingPerMonthPerCategory'  : print json_encode($render->yearSpendingPerMonthPerCategory("1", $category, $year)); break;
+        case 'yearOnYear'                       : print json_encode($render->yearOnYear("1", $year, $type)); break;
+        case 'incomeVsSpending'                 : print json_encode($render->incomeVsSpending("1", $year)); break;
+        case 'other' : break;// do something;break;
             // other cases
     }
 }
